017e8119629ad657e006c02b80991c47a8b246c2
Heliot <heliotcontacto@gmail.com>
2025-05-27 23:34:39 +0100

    fix: You can see the current route in the header section

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth-service.service';
 import { VideoManagerService } from '../services/video-manager.service';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -15,9 +16,18 @@ export class HeaderComponent {
   dropdownOpen = false;
   menuOpen = false;
   userID: number = 0;
+  activeRoute: string = '';
 
   constructor(public authService: AuthService, private router: Router, private el: ElementRef, private videoManager: VideoManagerService) {
     this.userID = this.videoManager.ensureAuthenticated();
+  }
+
+  goTo(route: string){
+    if (route == 'homepage') this.activeRoute = '';
+    this.activeRoute = route;
+    this.router.navigate([route]);
+    this.dropdownOpen = false;
+    this.menuOpen = false;
   }
 
   openDropdown(event: Event): void {
